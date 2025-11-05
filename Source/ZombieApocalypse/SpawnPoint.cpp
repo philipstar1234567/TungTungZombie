0d9e131667c6b9bef0a68a801ff1802aa8cb326f
Kirbywithacoolhat <gransethnerden.gaming@gmail.com>
2025-11-05 13:09:53 +0100

    Creates spawnpoint class

---
+++ Source/ZombieApocalypse/SpawnPoint.cpp
@@ -0,0 +1,27 @@
+// Copyright University of Inland Norway
+
+
+#include "SpawnPoint.h"
+
+// Sets default values
+ASpawnPoint::ASpawnPoint()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASpawnPoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASpawnPoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
