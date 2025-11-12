c3f476dbd765351b94665461a8ac74ad70795e05
Kirbywithacoolhat <gransethnerden.gaming@gmail.com>
2025-11-12 15:46:40 +0100

    Revert "Merge branch 'main' into kirby"
    This reverts commit ad963a9c96a1748cb8058d40678138335e4c7551, reversing
    changes made to 0d9e131667c6b9bef0a68a801ff1802aa8cb326f.

--- Source/ZombieApocalypse/SimulationController.h
+++ Source/ZombieApocalypse/SimulationController.h
@@ -51,7 +51,7 @@ public:
 
 	// ----- CONSTANTS / INITIALIZATION -----
 	const float days_to_become_infected_from_bite {15.f};   // Days
-	const float Bitten_capacity {10000.f};                    // Capacity inside conveyor == people at start
+	const float Bitten_capacity {100.f};                    // Capacity inside conveyor == people at start
 	const float patient_zero {1.0f};                        // Zombies
 	const float CONVERSION_FROM_PEOPLE_TO_ZOMBIES {1.f};    // Conversion constant, always 1
 	const float normal_number_of_bites {1.f};               // People/zombie/day
@@ -80,11 +80,11 @@ public:
 
 	//Susceptible (People) - choose a number that has a clean sqrt!
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Simulation Variables")
-	float Susceptible{ 100 };
+	int32 Susceptible{ 100 };
 	// Zombies = patient_zero
-	float Zombies{ 1 };		
+	int32 Zombies{ 1 };		
 	// Just to check if we are correctly updating stocks - used in SimulationHUD
-	float Bitten{ 0 };          // s Bitten -> in BittenArraySize
+	int32 Bitten{ 0 };          // s Bitten -> in BittenArraySize
 
 	// Conveyor content is the Bitten people incubating
 	// Will have one ConveyorBatch of each remainingDays
